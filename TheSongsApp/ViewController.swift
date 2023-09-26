3488349f5a0d5df2ae73b48bd6b7a0c61b26f63e
Frederico Lacis <fredlacispro@live.com>
2023-09-25 21:08:06 -0300

    Initial Commit

---
+++ TheSongsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSongsApp
+//
+//  Created by Frederico Lacis de Carvalho on 25/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
